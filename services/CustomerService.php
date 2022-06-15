4efc475597a31becd564fbeb6313f5677dba747e
hamid <hamidprime@gmail.com>
2022-06-15 12:57:30 +0430

    correct file path.

--- services/CustomerService.php
+++ services/CustomerService.php
@@ -1,7 +1,7 @@
 <?php
 
 include_once(_PS_MODULE_DIR_ . 'ps_hesabfa/services/LogService.php');
-include_once(_PS_MODULE_DIR_ . 'ps_hesabfa/services/SettingsService.php');
+include_once(_PS_MODULE_DIR_ . 'ps_hesabfa/services/SettingService.php');
 include_once(_PS_MODULE_DIR_ . 'ps_hesabfa/services/PsFaService.php');
 include_once(_PS_MODULE_DIR_ . 'ps_hesabfa/services/HesabfaApiService.php');
 
