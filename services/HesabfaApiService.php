1d906639dcf68371a9c5dd30fec3124d883a2fee
Sepehr <sepehrn249@gmail.com>
2024-05-20 15:28:47 +0330

    codes refactored

--- services/HesabfaApiService.php
+++ services/HesabfaApiService.php
@@ -334,6 +334,7 @@ class HesabfaApiService implements IHesabfaApiService
             'invoice' => $invoice,
         );
 
+        $this->saveStatistics();
         return $this->apiRequest($method, $data);
     }
 
@@ -517,5 +518,31 @@ class HesabfaApiService implements IHesabfaApiService
         }
     }
 
+    //================================================================================================
+    public function saveStatistics() {
+        $endpoint = "https://hesabfa.com/statistics/save";
+        $body = array(
+            "Platform" => "Prestashop",
+            "Website" => $_SERVER["HTTP_HOST"],
+            'APIKEY' => $this->settingService->getApiKey(),
+            "IP" => $_SERVER['REMOTE_ADDR']
+        );
+        $data_string = json_encode($body);
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $endpoint);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+            'Content-Type: application/json',
+            'Accept: application/json'
+        ));
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+    }
 
 }
